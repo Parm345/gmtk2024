0de63fcc41dfeaf5d2ca29a13b03220b4bac4a6c
Parm345 <51205078+Parm345@users.noreply.github.com>
2024-08-16 19:11:27 -0400

    pushing to git to continue working on my PC

--- game.gd
+++ game.gd
@@ -1,16 +1,49 @@
 extends Node2D
 
+onready var current_level:Node2D;
+onready var fader:AnimationPlayer = $AnimationPlayer;
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+signal level_changed;
+var levels = [];
+var next_level_index:int;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#levels
+	for i in range(GV.LEVEL_COUNT):
+		levels.append(load("res://Levels/Level"+str(i)+".tscn"));
+	add_level(GV.current_level_index);
 
+#defer this until previous level has been freed
+func add_level(n):
+	var level:Node2D = levels[n].instantiate();
+	add_child(level);
+	current_level = level;
+	
+	#fade in music
+	if current_level.has_node("BGM"):
+		var bgm = current_level.get_node("BGM");
+		bgm.fade_in();
+		bgm.play();
+	
+	emit_signal("level_changed");
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+#update current level and current level index
+func change_level(n):
+	if (n >= GV.LEVEL_COUNT):
+		return;
+	current_level.queue_free();
+	GV.current_level_score = 0;
+	
+	call_deferred("add_level", n);
+	GV.current_level_index = n;
+	
+func change_level_faded(n):
+	if (n >= GV.LEVEL_COUNT):
+		return;
+	next_level_index = n;
+	
+	#fade out
+	fader.play("fade_in_black");
+	if current_level.has_node("BGM"):
+		current_level.get_node("BGM").fade_out();
